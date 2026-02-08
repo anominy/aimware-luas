9cb4765de811fde1b0bded32657f6d91b3ed3183
anominy <140955338+anominy@users.noreply.github.com>
2026-02-08 14:56:59 +0300

    Add noblur.lua file

---
+++ noblur.lua
@@ -0,0 +1,24 @@
+local _g = {}
+_g.gui = gui
+_g.callbacks = callbacks
+_g.client = client
+
+local _def = {}
+_def.blur = 0
+
+local _cv = {}
+_cv.blur = "@panorama_disable_blur"
+
+local _call = {}
+_call.move = "CreateMove"
+_call.unload = "Unload"
+
+_g.callbacks.Register(_call.move, function()
+    if (tonumber(_g.client.GetConVar(_cv.blur)) == _def.blur) then
+        _g.client.SetConVar(_cv.blur, 1, true)
+    end
+end)
+
+_g.callbacks.Register(_call.unload, function()
+    _g.client.SetConVar(_cv.blur, 0, true)
+end)
